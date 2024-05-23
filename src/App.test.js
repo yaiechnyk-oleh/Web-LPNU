019ba764d7965f9ad725fde408a8474109d1536e
yaiechnyk-oleh <69356143+yaiechnyk-oleh@users.noreply.github.com>
2024-05-23 17:33:22 +0300

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
+// App.test.js
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders sign in button', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const signInButton = screen.getByRole('button', { name: /sign in/i });
+  expect(signInButton).toBeInTheDocument();
 });
